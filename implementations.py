c5a4a1f6e61d61b71b2403b91c5c81f28f8e6c3a
Tatar Dragos-Vlad <52969559+Dragos123321@users.noreply.github.com>
2023-10-06 17:15:19 +0200

    Added .idea in .gitignore and first implementation for mean_squared_error_sgd

--- implementations.py
+++ implementations.py
@@ -1,8 +1,31 @@
+import numpy as np
+
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     raise NotImplementedError
 
+
+def mean_squared_error(y, tx, w):
+    e = y - tx.dot(w)
+    N = len(y)
+
+    return (1 / (2 * N)) * np.sum(e ** 2)
+
+def stochastic_gradient_descent(y, tx, w):
+    N = len(y)
+    e = y - tx.dot(w)
+
+    return (-1 / N) * tx.dot(e)
+
 def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
-    raise NotImplementedError
+    w = initial_w
+
+    for i in range(max_iters):
+        loss = mean_squared_error(y, tx, w)
+        grad = stochastic_gradient_descent(y, tx, w)
+
+        w = w - gamma * grad
+
+    return w, loss
 
 def least_squares(y, tx):
     raise NotImplementedError
