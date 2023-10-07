71d2dd3175f64c89f9a21626933dbe5bdeeba4b2
Tatar Dragos-Vlad <52969559+Dragos123321@users.noreply.github.com>
2023-10-07 17:13:25 +0200

    Added __pycache__ to .gitignore and made all tests for mean_squared_error_sgd pass

--- implementations.py
+++ implementations.py
@@ -1,40 +1,51 @@
 import numpy as np
 
+
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     raise NotImplementedError
 
 
 def mean_squared_error(y, tx, w):
+    """Calculates the mean squared error"""
     e = y - tx.dot(w)
     N = len(y)
 
-    return (1 / (2 * N)) * np.sum(e ** 2)
+    return (1 / (2 * N)) * np.sum(e**2)
+
 
 def stochastic_gradient_descent(y, tx, w):
+    """Calculates the stochastic gradient descent"""
     N = len(y)
     e = y - tx.dot(w)
 
-    return (-1 / N) * tx.dot(e)
+    return (-1 / N) * tx.T.dot(e)
+
 
 def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
+    """Finds the best weights vector and loss for the given parameters using stochastic gradient descent"""
     w = initial_w
+    loss = mean_squared_error(y, tx, w)
 
     for i in range(max_iters):
-        loss = mean_squared_error(y, tx, w)
         grad = stochastic_gradient_descent(y, tx, w)
 
         w = w - gamma * grad
+        loss = mean_squared_error(y, tx, w)
 
     return w, loss
+
 
 def least_squares(y, tx):
     raise NotImplementedError
 
+
 def ridge_regression(y, tx, lambda_):
     raise NotImplementedError
 
+
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
     raise NotImplementedError
+
 
 def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
     raise NotImplementedError
