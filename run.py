f1e3ef96c98142dba7cfc8db75f05d6f8f97fed3
Anghul <maelyss.gilliard@epfl.ch>
2023-10-17 16:15:05 +0200

    data split and accuracy computation

--- run.py
+++ run.py
@@ -6,14 +6,33 @@ import helpers as hlp
 if __name__=="__main__":
     # Load the training data if present
     try:
-        x_train, y_train = hlp.load_csv_data("data/", sub_sample=False)
+        x, y = hlp.load_csv_data("data/", sub_sample=False)
     except:
         print("Training data not found. Please make sure you extracted the data.")
         exit(1)
     
     # Preprocess the data
-    x_train, y_train = hlp.preprocess_data(x_train, y_train, nan_rate_threshold=0.5, in_place=True)
+    x, y = hlp.preprocess_data(x, y, nan_rate_threshold=0.5, in_place=True)
+
+    # Split data to have 85% for training and 15% for testing to get accuracy
+    split_point = int(0.85*x.shape[0])
+
+    x_train = x[:split_point, :]
+    y_train = y[:split_point]
+
+    x_test = x[split_point:, :]
+    y_test = y[split_point:]
 
     #TODO: Train the model
     #TODO: Make predictions on the test set
+
+    # Result of passing x_train through the model, remove when predition done
+    y_result = np.array([])
+
+    accuracy = y_result[np.equal(y_test, y_result)].shape[0] / y_test.shape[0]
+    print(accuracy)
+
+
+
+
     
